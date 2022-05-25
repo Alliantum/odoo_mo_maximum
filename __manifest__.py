2112267c4c9885293aece195a25ebbf7fb7fdadb
Iago Alonso <iago.alonso@alliantum.com>
2022-05-25 17:03:58 +0200

    [IMP] Config files are more complete

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'author': "Alliantum",
     'website': "https://www.alliantum.com",
     'category': 'Manufacturing',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'depends': ['mrp', 'stock', 'product', 'mrp_production_grouped_by_product'],
     'data': [
         'views/product_views.xml',
